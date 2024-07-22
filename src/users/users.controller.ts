6ce17fe87303b196245a3898cefcfc8faba3f28a
Shahein Ockersz <shaheinockersz1234@gmail.com>
2024-07-22 23:17:56 +0530

    chore: Remove unused UsersService dependency in UsersController constructor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor() {}
 }
